3af1bbd9d1622f92f394715f21df844414a66c6b
cloay <cloay@localhost.(none)>
2012-10-15 21:19:06 +0800

    Initial Commit

---
+++ CloUICheckBox/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CloUICheckBox
+//
+//  Created by cloay on 12-10-15.
+//  Copyright (c) 2012年 cloay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CloUICheckBox/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CloUICheckBox
+//
+//  Created by cloay on 12-10-15.
+//  Copyright (c) 2012年 cloay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
